d055ccef7afe8291d62f9e1431b66f245119b1f8
Koch Christopher <koch.christopher@hotmail.fr>
2021-12-23 14:38:16 +0100

    fix card container

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import Portfolio from "./Portfolio"
 import  "./css/style.css"
 
 function App(){
-const [loading,setLoading]=useState(false)
+const [loading,setLoading]=useState(true)
 
-    // useEffect(() => {
-    //     setTimeout(()=>{setLoading(false)},4000);
+    useEffect(() => {
+        setTimeout(()=>{setLoading(false)},4000);
 
-    //     }, [])
+        }, [])
 
-    // useEffect(() => {
-    //     window.addEventListener("resize",()=>setLoading(true,setTimeout(()=>{setLoading(false)},4000)))
+    useEffect(() => {
+        window.addEventListener("resize",()=>setLoading(true,setTimeout(()=>{setLoading(false)},4000)))
        
-    //   },[])
+      },[])
 
     return (
         loading ? (<div className="front-container"><span className="front-container__hello">Hello<span className="front-container__hello__effect"><span className="front-container__hello__effect__opacity">|</span></span></span><br/></div>)
