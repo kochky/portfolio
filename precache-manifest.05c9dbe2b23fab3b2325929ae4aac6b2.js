54bb8b50855f754b905b948ef559ec46de0c673c
Koch Christopher <koch.christopher@hotmail.fr>
2021-12-23 11:58:04 +0100

    Updates

---
+++ precache-manifest.05c9dbe2b23fab3b2325929ae4aac6b2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0476321ada4ae27e8ab7aa296b232220",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2cd0663e4006e57dabb8",
+    "url": "/portfolio/static/css/main.162a1fd0.chunk.css"
+  },
+  {
+    "revision": "e7e6efd63dc7335305a8",
+    "url": "/portfolio/static/js/2.78ac08ff.chunk.js"
+  },
+  {
+    "revision": "fb03f3f1df115f19f086f8f420b9efa1",
+    "url": "/portfolio/static/js/2.78ac08ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd0663e4006e57dabb8",
+    "url": "/portfolio/static/js/main.91614c16.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "844324dc83d98980285a9ab8f3dcb5f0",
+    "url": "/portfolio/static/media/Aeonik-Regular.844324dc.otf"
+  },
+  {
+    "revision": "00aeed55ae8bac305fc2092db642dab7",
+    "url": "/portfolio/static/media/book.00aeed55.webp"
+  },
+  {
+    "revision": "b11eefa03178688f277397a578170b00",
+    "url": "/portfolio/static/media/heart.b11eefa0.webp"
+  },
+  {
+    "revision": "9f6d884d6e142bccf75a4c0323ad2c96",
+    "url": "/portfolio/static/media/profil.9f6d884d.jpeg"
+  },
+  {
+    "revision": "5da7a45495d5be209409b540c6b9d9f4",
+    "url": "/portfolio/static/media/qrcode.5da7a454.webp"
+  }
+]);
